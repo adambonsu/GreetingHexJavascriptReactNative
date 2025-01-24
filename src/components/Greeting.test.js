565950604ec50c2268604629c3ce25dce013939e
Adam Bonsu <adam@bonsu.io>
2025-01-24 19:18:56 +0000

    include component/Greeting.test.js

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,9 @@
+import renderer from "react-test-renderer";
+import Greeting from "./Greeting";
+
+describe("App", () => {
+  it("renders with the correct content", () => {
+    const component = renderer.create(<Greeting />).toJSON();
+    expect(component.children[0].children[0]).toBe("Yagga!");
+  });
+});
